0a8261912736a4f0509e21e5ac6ab90f7e645d56
Ties <73988697+TSpeel@users.noreply.github.com>
2025-06-11 16:16:34 +0200

    Update arm_gic.c

--- hw/intc/arm_gic.c
+++ hw/intc/arm_gic.c
@@ -93,13 +93,13 @@ void gic_update(GICState *s)
             if (GIC_TEST_ENABLED(irq, cm) && gic_test_pending(s, irq, cm) &&
                 (irq < GIC_INTERNAL || GIC_TARGET(irq) & cm)) {
                 int current_prio = GIC_GET_PRIORITY(irq, cpu);
-                if (irq == 35) {
-                    qemu_log_mask(LOG_GUEST_ERROR,
-                                  "GIC_HACK: Forcing IRQ priority from 0x%x to 0xA0\n",
-                                  current_prio);
-                    current_prio = 0xA0;
-                }
-                if (current_prio < best_prio && irq != 34) {
+                //if (irq == 35) {
+                //    qemu_log_mask(LOG_GUEST_ERROR,
+                //                  "GIC_HACK: Forcing IRQ priority from 0x%x to 0xA0\n",
+                //                  current_prio);
+                //    current_prio = 0xA0;
+                //}
+                if (current_prio < best_prio) { //&& irq != 34) {
                     best_prio = current_prio;
                     best_irq = irq;
                 }
