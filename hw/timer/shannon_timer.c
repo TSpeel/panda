268b95c6d786d9c1e04e2a755de618912a26ab12
Ties <73988697+TSpeel@users.noreply.github.com>
2025-06-25 09:21:53 +0200

    Enabled timer debug flag

--- hw/timer/shannon_timer.c
+++ hw/timer/shannon_timer.c
@@ -18,7 +18,7 @@
 #include "hw/cpu/a9mpcore.h"
 #include "hw/cpu/a15mpcore.h"
 
-#define SH_TIMER_DEBUG_GATE 0
+#define SH_TIMER_DEBUG_GATE 1
 
 #define DPRINTF(fmt, ...) do {                                          \
         if (SH_TIMER_DEBUG_GATE) {                                           \
